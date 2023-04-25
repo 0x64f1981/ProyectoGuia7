e08bbaeb2711ba281fc41b0d90cec61d1dbd16ea
Fabio <iammasterpvp333@gmail.com>
2023-04-25 17:03:23 -0600

    Add project files.

---
+++ ProyectoGuia7/CalcularUtilidad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProyectoGuia7
+{
+    class CalcularUtilidad
+    {
+        public static double ImprimirError(double ta, double s)
+        {
+            double u = 0;
+            if (ta <= 0 && s < 0)
+                Console.WriteLine("Datos incorrectos. Porfavor, reinicie el programa.");
+
+            if (ta < 1)
+                u = s * 0.05;
+            else
+                if (ta < 2)
+                u = s * 0.07;
+            else
+                if (ta < 5)
+                u = s * 0.1;
+            else
+                if (ta <= 10)
+                u = s * 0.15;
+            else
+                if (ta > 20)
+                u = s * 0.2;
+            return u;
+        }
+    }
+}
---
+++ ProyectoGuia7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProyectoGuia7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definición de variables
+            double ta, s;
+
+            //Identificación del programa
+            Console.WriteLine("Calcular utilidad de un empleado en base a los años trabajados y su salario.");
+
+            //Entrada de datos
+            Console.Write("Ingresa el tiempo trabajado en la empresa (en años): ");
+            ta = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa tu salario (en USD): $");
+            s = double.Parse(Console.ReadLine());
+
+            //Procesamiento de datos
+            double u = CalcularUtilidad.ImprimirError(ta, s);
+
+            //Output
+            PrintValues(ta, s, u);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Se encarga de mostrar en pantalla los valores introducidos.
+        /// </summary>
+        /// <param name="ta">Se refiere a Tiempo trabajado en los años.</param>
+        /// <param name="s">Se refiere al Salario mensual</param>
+        /// <param name="u">U es la utilidad</param>
+        static void PrintValues(double ta, double s, double u)
+        {
+            Console.WriteLine("\nTu salario mensual es de: {0}\n" +
+                "Tiempo trabajado en la empresa en años: {1}\n" +
+                "La utilidad del empleado es de: ${2}", s, ta, u);
+        }
+    }
+}
